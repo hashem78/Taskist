900a7ae1f0630a502446b917ae374b1cc3b4ed53
hashem78 <hashem.olayano@gmail.com>
2020-09-20 18:15:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/Taskist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Taskist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kprimaryDarkColor = const Color(0xFF689F38);
+const kprimaryLightColor = const Color(0xFFDCEDC8);
+const kprimaryColor = const Color(0xFF8BC34A);
+const kTextColor = const Color(0xFF212121);
+const kaccentColor = const Color(0xFF4CAF50);
+const kprimaryTextColor = const Color(0xFF212121);
+const ksecondaryTextColor = const Color(0xFF757575);
+const kdividerColor = const Color(0xFFBDBDBD);
