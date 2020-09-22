0bbd434648f33890acc57cb4253cb49b178258d5
hashem78 <hashem.olayano@gmail.com>
2020-09-22 10:57:32 +0300

    Replace container with cliprrect 4 inkewll effects

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -8,57 +8,53 @@ class TaskTile extends StatelessWidget {
   TaskTile({this.title, this.priorityColor});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: priorityColor,
-        borderRadius: BorderRadius.all(
-          Radius.circular(25),
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.all(
+        Radius.circular(50),
       ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: CircleAvatar(
-              backgroundColor: Colors.red,
-              child: Text(
-                title[0].toUpperCase(),
-                style: TextStyle(
-                  color: kTextColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+      child: Material(
+        color: priorityColor,
+        child: InkWell(
+          onTap: () {},
+          splashColor: Colors.grey,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: CircleAvatar(
+                  backgroundColor: Colors.red,
+                  child: Text(
+                    title[0].toUpperCase(),
+                    style: TextStyle(
+                      color: kTextColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                  radius: 30,
                 ),
               ),
-              radius: 30,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: kprimaryDarkColor,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(25),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Text(
-                  title,
-                  maxLines: 1,
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w300,
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    title,
+                    maxLines: 1,
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ),
               ),
-            ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
           ),
-          SizedBox(
-            width: 10,
-          ),
-        ],
+        ),
       ),
     );
   }
