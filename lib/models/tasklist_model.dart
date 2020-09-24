9d68b3d29cc55a7f3772c488f5d616f5af1c5339
hashem78 <hashem.olayano@gmail.com>
2020-09-24 18:55:36 +0300

    Remove useless prints

--- lib/models/tasklist_model.dart
+++ lib/models/tasklist_model.dart
@@ -27,7 +27,6 @@ class TaskListModel extends ChangeNotifier {
       for (var value in decoded.entries) {
         tasks[value.key] = TaskModel.fromJson(value.value);
       }
-      print("done for looping");
     } catch (e) {
       print(e.toString());
     }
@@ -40,7 +39,6 @@ class TaskListModel extends ChangeNotifier {
     var dir = await getApplicationDocumentsDirectory();
     File store = File(join(dir.path, "database.json"));
     await store.writeAsString(encoded, mode: FileMode.write);
-    print("saved tasks");
   }
 
   void addTask(TaskModel newTask) async {
@@ -56,7 +54,6 @@ class TaskListModel extends ChangeNotifier {
 
   bool removeTask(String id) {
     if (contains(id)) {
-      print("removed $id");
       tasks.remove(id);
       notifyListeners();
       saveTasks();
